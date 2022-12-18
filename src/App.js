c9ddef7313bb272ab0e8b6d95a02ad7098e83632
siddharth9300 <jkrish930@gmail.com>
2022-12-19 00:35:27 +0530

    added popular movies section

--- src/App.js
+++ src/App.js
@@ -17,6 +17,63 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [favourites, setFavourites] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const[popmovies, SetPopMovies] = useState([]);
+
+
+  const getPopRequest = async () => {
+    // const url = 'https://most-popular-movies-right-now-daily-update.p.rapidapi.com/';
+
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'bf11fd3266msh419a46d02d0ef99p1e6ecajsnf2925e9766a2',
+        'X-RapidAPI-Host': 'most-popular-movies-right-now-daily-update.p.rapidapi.com'
+      }
+    };
+
+    fetch('https://most-popular-movies-right-now-daily-update.p.rapidapi.com/',options)
+    .then(response => response.json())
+    
+    .then(response => SetPopMovies(response))
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+    // const responseJson = await response.json();
+    // console.log(response.Search);
+    // if (response.Search) {
+    //   SetPopMovies(response.Search);
+    //   console.log(response.Search);
+    // }
+  };
+
+  useEffect(() => {
+
+      getPopRequest();
+ 
+  }, []);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
   const getMovieRequest = async (searchValue) => {
@@ -83,6 +140,19 @@ function App() {
                   searchValue={searchValue}
                   setSearchValue={setSearchValue}
                 />
+                <MoviesListHeading heading="Popular" />
+                <MoviesList
+                  movies={popmovies}
+                  handleFavouritesClick={addFavouritesMovie}
+                  favouriteComponent={AddFavourites}
+                />
+                {/* <Popular 
+                  movies={popmovies}
+                  handleFavouritesClick={addFavouritesMovie}
+                  // popularComponent={getPopularMovies}
+                  /> */}
+
+                {/* Movie list */}
                 <MoviesListHeading heading="Movies" />
                 <MoviesList
                   movies={movies}
